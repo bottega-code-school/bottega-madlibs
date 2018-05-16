3953d53e3b547073fbb6d2e73851b4500f883e6a
Max <maxnelson997@gmail.com>
2018-05-16 15:33:09 -0600

    changed port and app title

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Bottega Madlibs</h1>
       </div>
     );
   }
