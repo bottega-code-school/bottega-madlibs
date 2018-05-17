c0ccb1536fe2bae99e94bea1d11be7558251e90d
Max <maxnelson997@gmail.com>
2018-05-16 23:08:19 -0600

    put number labels on inputs

--- src/components/card.js
+++ src/components/card.js
@@ -76,7 +76,9 @@ class Card extends Component {
             <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card__inputs">
                 {
-                    inputData.map(data => Input( (data), this.handleInputChange ))
+                    inputData.map((data, index) => {
+                        return Input( (data), this.handleInputChange, index) 
+                    })
                 }
                 </div>
                 <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
--- src/components/content.js
+++ src/components/content.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 class Content extends Component {
     render() {
         const data = this.props.data;
+        for(key in data) {
+            console.log(key);
+        }
+        // this.props.data.map((object,index) => {
+        //     console.log(index);
+        // })
+        // <span>{data.indexOf(data.color)}</span>
         return (
             <p>
               Ladies and gentlemen, this is <b>{data.color}</b> Barber, your sportscaster, bringing you the last inning of the game between the Cleveland <b>{data.pluralNoun}</b> and the <b>{data.adjectiveOne}</b> Yankees. <b>{data.celebOne}</b> is pitching for the Yankees. Here's the pitch! It's a low <b>{data.adjectiveTwo}</b> ball that just cuts the inside of the <b>{data.nounOne}</b> for a strike. That makes the count <b>{data.numberOne}</b> strikes and <b>{data.numberTwo}</b> balls. Now here's the next pitch. The batter swings and connects. It's a long, high <b>{data.nounTwo}</b> out to <b>{data.adjectiveThree}</b> field. But <b>{data.celebTwo}</b> is coming up fast and has it for the second out. The next batter up is <b>{data.celebThree}</b>, the Cleveland <b>{data.adjectiveFour}</b>-stop. Here's the pitch... and it's hit... a short ground ball to third <b>{data.nounThree}</b>. <b>{data.celebFour}</b> scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the <b>{data.adjectiveFive}</b> League!  
