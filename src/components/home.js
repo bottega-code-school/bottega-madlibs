7868a227a8115f6221f69cb228155520c1ca6202
Max <maxnelson997@gmail.com>
2018-05-17 15:19:27 -0600

    styled content component

--- src/components/home.js
+++ src/components/home.js
@@ -14,4 +14,10 @@ class Home extends Component {
   }
 }
 
+// things we need to fix
+// placeholder
+// gray and green number labels
+// content labels
+// generate btn space/card-height
+
 export default Home;
