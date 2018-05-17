8077df12396905786a9d9cd5aca67d17ff7c91b8
Max <maxnelson997@gmail.com>
2018-05-16 23:30:39 -0600

    created a css grid for card and inputs

--- src/components/content.js
+++ src/components/content.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 class Content extends Component {
     render() {
         const data = this.props.data;
-        for(key in data) {
-            console.log(key);
-        }
         // this.props.data.map((object,index) => {
         //     console.log(index);
         // })
